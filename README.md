f42eee37a39c8d1d154368edb7f54af6bf18d413
John Honniball <coredump@virginmedia.com>
2024-05-31 23:23:48 +0100

    Initial commit of code and README.

---
+++ README.md
@@ -0,0 +1,12 @@
+![Static Badge](https://img.shields.io/badge/MCU-ATmega328-green "MCU:ATmega328")
+![Static Badge](https://img.shields.io/badge/BOARD-Arduino-green "BOARD:Arduino")
+
+# SNESPadTester #
+
+Small test program to read the button states from a Super Nintendo video game controller.
+
+## Connections ##
+
+Pin 2: clock, pin 3: latch, pin 4: data.
+
+
---
+++ SNESPadTester.ino
@@ -0,0 +1,109 @@
+/* SNESPadTester --- test program for the SNES game controller 2024-05-31 */
+/* Copyright (c) 2024 John Honniball */
+
+/* Released under the GNU Public Licence (GPL) */
+
+// Connections to SNES controller
+#define CLOCK_PIN    2
+#define LATCH_PIN    3
+#define DATA_PIN     4
+
+// Direct port I/O defines for Arduino with ATmega328
+// Change these if running on Mega Arduino
+#define SNESOUT      PORTD
+#define SNESIN       PIND
+#define LATCH_BIT    (1 << LATCH_PIN)
+#define CLOCK_BIT    (1 << CLOCK_PIN)
+#define DATA_BIT     (1 << DATA_PIN)
+
+// Bits returned by 'readSNES':
+#define NES_A        1
+#define NES_B        2
+#define NES_SELECT   4
+#define NES_START    8
+#define NES_N       16
+#define NES_S       32
+#define NES_W       64
+#define NES_E      128
+
+
+void setup(void)
+{
+  Serial.begin(9600);
+  
+  Serial.println("SNES Pad Tester");
+  Serial.println("John Honniball, May 2024");
+  
+  // Set up I/O pins for the SNES controller
+  pinMode(LATCH_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(DATA_PIN, INPUT);
+  
+  digitalWrite(LATCH_PIN, LOW);
+  digitalWrite(CLOCK_PIN, LOW);
+}
+
+
+void loop(void)
+{
+  for (;;) {
+    const unsigned int snesbits = readSNES();
+
+    Serial.print(snesbits, BIN);
+    Serial.print(" ");
+    Serial.print(snesbits, DEC);
+    Serial.println();
+
+    delay(20);
+  }
+
+}
+
+
+/* readSNES --- read the SNES controller */
+
+unsigned int readSNES(void)
+{
+  // With digitalRead/digitalWrite: 150us
+  // With direct port I/O: 24us
+//unsigned long int before, after;
+  unsigned int snesbits = 0;
+  int i;
+  
+//before = micros();
+  
+#ifdef SLOW_READSNES
+  digitalWrite(LATCH_PIN, HIGH);
+  digitalWrite(LATCH_PIN, LOW);
+  
+  for (i = 0; i < 12; i++) {
+    if (digitalRead(DATA_PIN) == LOW)
+      snesbits |= (1 << i);
+      
+    digitalWrite(CLOCK_PIN, HIGH);
+    digitalWrite(CLOCK_PIN, LOW);
+  }
+#else
+  SNESOUT |= LATCH_BIT;
+  delayMicroseconds(2);
+  SNESOUT &= ~LATCH_BIT;
+  
+  for (i = 0; i < 12; i++) {
+    delayMicroseconds(2);
+    
+    if ((SNESIN & DATA_BIT) == 0)
+      snesbits |= (1 << i);
+      
+    SNESOUT |= CLOCK_BIT;
+    delayMicroseconds(2);
+    SNESOUT &= ~CLOCK_BIT;
+  }
+#endif
+  
+//after = micros();
+//Serial.print("readSNES: ");
+//Serial.print(after - before, DEC);
+//Serial.println("us");
+  
+  return (snesbits);
+}
